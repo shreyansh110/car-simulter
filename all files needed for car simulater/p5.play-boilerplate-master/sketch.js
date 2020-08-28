22bc51a54693a5478009c8f522d13c8a2f9a81a6
shreyansh110 <69755759+shreyansh110@users.noreply.github.com>
2020-08-28 14:40:43 +0530

    Add files via upload

--- all files needed for car simulater/p5.play-boilerplate-master/sketch.js
+++ all files needed for car simulater/p5.play-boilerplate-master/sketch.js
@@ -1,29 +1,47 @@
-var car,wall;
+var car;
+var wall;
 var deformation=0.5*weight*speed*speed/22509
-var speed,weight;
+
 
 
 function setup() {
   createCanvas(1000,400);
 car=createSprite(950,200,50,30);
-wall=createSprite(10,200,10,400)
+wall=createSprite(10,200,10,400);
+speed=random(-55,-90);
+weight=random(3500,4500)
 }
 
 function draw() {
   background(80,500,255);  
 
-  car.velocityX=-9;
+  car.velocityX=speed;
  car.debug=true;
  wall.debug=true;
  car.depth=wall.depth;
-if(wall.x-car.x<car.width/2+wall.widhth/2){
+if(wall.x-car.x>(wall.width+car.width)/2){
 car.velocityX=0;
 car.shapeColor="red"
 }
 
+if(deformation<100){
+
+car.shapeColor=color(0,225,0)
 
 
+}
 
+if(deformation>100&&deformation<180){
+
+
+car.shapeColor=color(230,230,0)
+}
+
+if(deformation>180){
+
+
+  car.shapeColor=color(225,0,0)
+  }
 
 drawSprites();
   
